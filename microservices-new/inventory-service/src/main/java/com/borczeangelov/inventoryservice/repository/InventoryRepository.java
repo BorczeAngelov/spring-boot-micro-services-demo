82aa01d6f885c5f1de3274eb33cf4aeec3d9c8bc
Borcze Angelov <Borcze.Angelov@yahoo.com>
2022-06-06 15:27:01 +0200

    inter service communication

--- microservices-new/inventory-service/src/main/java/com/borczeangelov/inventoryservice/repository/InventoryRepository.java
+++ microservices-new/inventory-service/src/main/java/com/borczeangelov/inventoryservice/repository/InventoryRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.borczeangelov.inventoryservice.model.Inventory;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    Optional<Inventory> findBySkuCode(String skuCode);
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
--- microservices-new/order-service/src/main/java/com/borczeangelov/orderservice/service/OrderService.java
+++ microservices-new/order-service/src/main/java/com/borczeangelov/orderservice/service/OrderService.java
@@ -1,10 +1,13 @@
 package com.borczeangelov.orderservice.service;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import com.borczeangelov.orderservice.dto.InventoryResponse;
 import com.borczeangelov.orderservice.dto.OrderLineItemsDto;
 import com.borczeangelov.orderservice.dto.OrderRequest;
 import com.borczeangelov.orderservice.model.Order;
@@ -18,10 +21,23 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class OrderService {
 
-    private final OrderRepositroy orderRepositroy;
+    private final OrderRepositroy orderRepository;
+    private final WebClient webClient;
 
     public void placceOrder(OrderRequest orderRequest) {
-        Order order = new Order();
+        var order = CreateOrder(orderRequest);
+
+        var areAllProductsInStock = AreProductsInStock(order);
+
+        if (areAllProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
+    }
+
+    private Order CreateOrder(OrderRequest orderRequest) {
+        var order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
         var orderLineItems = orderRequest.getOrderLineItemsDtoList()
@@ -30,8 +46,7 @@ public class OrderService {
                 .toList();
 
         order.setOrderLineItemsList(orderLineItems);
-
-        orderRepositroy.save(order);
+        return order;
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
@@ -40,5 +55,27 @@ public class OrderService {
         orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
         orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
         return orderLineItems;
+    }    
+
+    /*
+     * Make a HTTP Get request to InventoryService and
+     * check if all products are in stock
+     */
+    private boolean AreProductsInStock(Order order) {
+        var skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        var inventoryResponsArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build()) //?skuCode=iphone-13&skuCode=iphone13-red
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class) //parse the object to InventoryResponse[]
+                .block();
+
+        var allProductsInStock = Arrays.stream(inventoryResponsArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        return allProductsInStock;
     }
 }
